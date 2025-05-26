bd5c06c6e538cf7a2c52be30c706f1a6054592d5
Lcs-ns <lucas.nas314@gmail.com>
2025-05-26 11:08:16 -0300

    new dataset

--- yolo_.py
+++ yolo_.py
@@ -2,4 +2,4 @@ from ultralytics import YOLO
 
 model = YOLO('yolov8n.pt')
 
-model.train(data='datasets/data.yaml', epochs=50, imgsz=640)
+model.train(data='datasets_2/data.yaml', epochs=50, imgsz=640)
